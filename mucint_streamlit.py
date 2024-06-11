f276aa6e60000ad92e03ca700b16ce3114656e45
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-06-11 13:15:07 +0000

    Update mucint_streamlit.py

--- mucint_streamlit.py
+++ mucint_streamlit.py
@@ -106,148 +106,24 @@ if submit_button:
         process3 = subprocess.Popen(["Rscript", "predict.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         result3 = process3.communicate()
                                            
-                    df2 = pd.read_csv(r'fin_results2.csv')
-                    df2 = df2.rename(columns={0: "POL", 1: "DF", 2: "LC", 3: "LE"})
-            
-                    SDc = ((df2["DF"])*((df2["LE"])/100))
-                    SDc2 = (((df2["LC"]/100)*(-1)*10)/((df2["LC"]/100)-1))
-                    SDcx = ((SDc+SDc2)/2)
+        df2 = pd.read_csv(r'results.csv')
+        st.write(head(df2))
                     
-                    if len(SMI2) > 2:
-                        SDc = ((df2["DF"])*((df2["LE"])/100))
-                        SDc2 = (((df2["LC"]/100)*(-1)*10)/((df2["LC"]/100)-1))
-                        SDcx = ((SDc+SDc2)/2)
-                    
-            
-                    calcLE=(SDcx/df2["DF"])*100
-                    calcLC=(SDcx/(SDcx+10))*100
-                    
-                    df3={'POL' : df2["POL"], 'DF' : df2["DF"], 'SD': SDcx}
-                    df3=pd.DataFrame(df3,columns=["POL","DF","SD"])
-                    
-                    df4={'POL' : df2["POL"], 'DF' : df2["DF"], 'SD': SDc}
-                    df4=pd.DataFrame(df4,columns=["POL","DF","SD"])
-            
-                    df5={'POL' : df2["POL"], 'DF' : df2["DF"], 'SD': SDc2}
-                    df5=pd.DataFrame(df5,columns=["POL","DF","SD"])
-            
-                    df6={'POL' : df2["POL"], 'DF' : df2["DF"], 'LE': calcLE,'LC': calcLC}
-                    df6=pd.DataFrame(df6,columns=["POL","DF","LE","LC"])
-                    
-                    custom_palette = sns.color_palette("deep")
-            
-                    max_indexes = SDcx[SDcx == max(SDcx)].index.tolist()
-                    
-                    col1, col2 = st.columns(2)
-            
-                    finalLE=round((round(max(SDcx),1)/df3.loc[SDcx.idxmax(), "DF"])*100,0)
-                    finalLC=round((round(max(SDcx),1)/(df3.loc[SDcx.idxmax(), "DF"]+10))*100,0)
-    
-                    finalLE2=round((round(max(SDc),1)/df4.loc[SDc.idxmax(), "DF"])*100,0)
-                    finalLC2=round((round(max(SDc),1)/(df4.loc[SDc.idxmax(), "DF"]+10))*100,0)
-    
-                    finalLE3=round((round(max(SDc2),1)/df5.loc[SDc2.idxmax(), "DF"])*100,0)
-                    finalLC3=round((round(max(SDc2),1)/(df5.loc[SDc2.idxmax(), "DF"]+10))*100,0)
-                    
-                    with col1: 
-                        st.header("Formulation report")
-                        st.write("Maximum solubilized drug: "+str(round(max(SDcx),1))+" g/L at "+str(df3.loc[SDcx.idxmax(), "DF"])+" g/L drug feed (LE: "+str(finalLE)+" %, LC: "+str(finalLC)+" %)")
-                        max_values = df3.groupby('POL')['SD'].max()
-                        max_value = max_values.max()
-                        keys_with_max_value = max_values[max_values == max_value].index.tolist()
-                        comma_separated_keys = ', '.join(str(key) for key in keys_with_max_value)
-                        st.write(comma_separated_keys)
+        with col1: 
+            st.header("Formulation report")
+            st.write("Maximum solubilized drug: "+str(round(max(SDcx),1))+" g/L at "+str(df3.loc[SDcx.idxmax(), "DF"])+" g/L drug feed (LE: "+str(finalLE)+" %, LC: "+str(finalLC)+" %)")
+            max_values = df3.groupby('POL')['SD'].max()
+            max_value = max_values.max()
+            keys_with_max_value = max_values[max_values == max_value].index.tolist()
+            comma_separated_keys = ', '.join(str(key) for key in keys_with_max_value)
+            st.write(comma_separated_keys)
             
                             
-                    with col2:
-                        im = Draw.MolToImage(Chem.MolFromSmiles(SMI),fitImage=True)
-                        st.image(im)
+        with col2:
+            im = Draw.MolToImage(Chem.MolFromSmiles(SMI),fitImage=True)
+            st.image(im)
             
-                    st.write("Predicted amount of solubilized drug (average by LE and LC models, see below)")
-                    fig3=plt.figure(figsize=(10, 6))
-                    ax=sns.barplot(x="DF", y="SD", hue="POL", data=df3)
-                    plt.xlabel("Drug feed [g/L]")
-                    plt.ylabel("Solubilized drug [g/L]")
-                    plt.ylim(0, 10)
-                    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-                    st.pyplot(fig3)
-            
-                    col1, col2 = st.columns(2)
-                    with col1:
-                        st.write("Amount based on LE models:")                   
-                        st.write("Maximum solubilized drug: "+str(round(max(SDc),1))+" g/L at "+str(df4.loc[SDc.idxmax(), "DF"])+" g/L drug feed (LE: "+str(finalLE2)+" %, LC: "+str(finalLC2)+" %)")
-                        max_values = df4.groupby('POL')['SD'].max()                    
-                        max_value = max_values.max()
-                        keys_with_max_value = max_values[max_values == max_value].index.tolist()
-                        comma_separated_keys = ', '.join(str(key) for key in keys_with_max_value)
-                        st.write(comma_separated_keys)
-                        
-                        fig1a=plt.figure(figsize=(10, 6))
-                        ax = sns.barplot(x="DF", y="SD", hue="POL", data=df4)
-                        plt.xlabel("Drug feed [g/L]")
-                        plt.ylabel("Solubilized drug [g/L]")
-                        plt.ylim(0, 10)
-                        ax.get_legend().remove()
-                        st.pyplot(fig1a)
-            
-                        st.write("Calculated from LE predictions:")
-                        fig1b=plt.figure(figsize=(10, 6))
-                        ax = sns.barplot(x="DF", y="LE", hue="POL", data=df2)
-                        plt.xlabel("Drug feed [g/L]")
-                        plt.ylabel("Ligand efficiency [%]")
-                        plt.ylim(0, 100)
-                        ax.get_legend().remove()
-                        st.pyplot(fig1b)
-                    
-                    
-                    with col2:
-                        st.write("Amount based on LC models:")
-                        st.write("Maximum solubilized drug: "+str(round(max(SDc2),1))+" g/L at "+str(df5.loc[SDc2.idxmax(), "DF"])+" g/L drug feed (LE: "+str(finalLE3)+" %, LC: "+str(finalLC3)+" %)")
-                        max_values = df5.groupby('POL')['SD'].max()                    
-                        max_value = max_values.max()
-                        keys_with_max_value = max_values[max_values == max_value].index.tolist()
-                        comma_separated_keys = ', '.join(str(key) for key in keys_with_max_value)
-                        st.write(comma_separated_keys)
-                        fig2a=plt.figure(figsize=(10, 6))
-                        ax = sns.barplot(x="DF", y="SD", hue="POL", data=df5)
-                        plt.xlabel("Drug feed [g/L]")
-                        plt.ylabel("Solubilized drug [g/L]")
-                        plt.ylim(0, 10)
-                        ax.get_legend().remove()
-                        st.pyplot(fig2a)
-            
-                        st.write("Calculated from LC predictions:")
-                        fig2b=plt.figure(figsize=(10, 6))
-                        ax = sns.barplot(x="DF", y="LC", hue="POL", data=df2)
-                        plt.xlabel("Drug feed [g/L]")
-                        plt.ylabel("Loading capacity [%]")
-                        plt.ylim(0, 50)
-                        ax.get_legend().remove()
-                        st.pyplot(fig2b)
-            
-                    df = pd.read_csv(r'fin_results.csv',index_col=0)
-                    df = df.rename(columns={0: "POL", 1:"DF", 2: "LC10", 3: "LC20", 4: "LC30", 5: "LC40", 6: "LE20", 7: "LE40", 8: "LE60", 9: "LE80", 10:"Passed"})
-                    df.reset_index(inplace=True)
-    
-                    st.write("Table of all predictions:")
-                    st.dataframe(df.style.applymap(cooling_highlight,subset=["LC10","LC20","LC30","LC40","LE20","LE40","LE60","LE80","Passed"]))     
-                    
-                    st.write("Amount of passed thresholds:")
-                    fig4=plt.figure(figsize=(10, 6))
-                    ax=sns.barplot(x="DF", y="Passed", hue="POL", data=df)
-                    plt.xlabel("Drug feed [g/L]")
-                    plt.ylabel("Thresholds passed")
-                    plt.ylim(0, 8)
-                    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-                    st.pyplot(fig4)
-
-                
-                df = pd.read_csv(r'fin_results.csv',index_col=0)
-                df = df.rename(columns={0: "POL", 1: "DRUG", 2:"DF", 3: "LC10", 4: "LC20", 5: "LC30", 6: "LC40", 7: "LE20", 8: "LE40", 9: "LE60", 10: "LE80", 11:"Passed"})
-                df.reset_index(inplace=True)               
-                st.dataframe(df.style.applymap(cooling_highlight,subset=["LC10","LC20","LC30","LC40","LE20","LE40","LE60","LE80","Passed"]))    
-    
-        for es in ["descriptors.csv","options.csv","create_formulations_temp.R","fin_results.csv","fin_results2.csv","db_test.csv","testformulations.dat","db_formulations.csv"]:
+        for es in ["descriptors.csv","results.csv"]:
             try:
                 os.remove(es)
             except:

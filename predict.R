d83868669678ac878393b24a285253646c8e39ca
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-06-11 13:38:48 +0000

    Update predict.R

--- predict.R
+++ predict.R
@@ -1,6 +1,6 @@
 library("caret")
 
-data=read.table("descriptors.csv")
+data=read.table("descriptors.csv",header=T)
 print(colnames(data))
 load("finmodel_9.rda")
 write.csv("results.csv",predict(final_model2,data))

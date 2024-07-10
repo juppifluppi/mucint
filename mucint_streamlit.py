06647a311d867ef8d333319a1a6e103a1cbb71ba
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-07-10 18:11:00 +0000

    Update mucint_streamlit.py

--- mucint_streamlit.py
+++ mucint_streamlit.py
@@ -121,7 +121,7 @@ if submit_button:
                     f.write(str(maccskeys[o])+"\t") 
                 f.write("\n")
                                                                                      
-            subprocess.Popen(["Rscript", "predict.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            st.write(subprocess.Popen(["Rscript", "predict.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True))
             process3.communicate()
            
             df2 = pd.read_csv(r'results.csv')

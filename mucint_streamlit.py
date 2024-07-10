bd5c74fed03eb6af8379363b2597a491c081bdac
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-07-10 17:46:53 +0000

    Update mucint_streamlit.py

--- mucint_streamlit.py
+++ mucint_streamlit.py
@@ -184,7 +184,7 @@ if submit_button:
            dfx["MUC2 interaction probability"]=dfx.iloc[:, 1].astype(int)
 
            dfx["+bile"]=(df3.iloc[:, 0].astype(float))*100
-           #dfx["+bile"]=dfx.iloc[:, 2].astype(int)
+           dfx["+bile"]=dfx.iloc[:, 2].astype(int)
     
            #dfx.reset_index(inplace=True)               
            st.dataframe(dfx.style.applymap(cooling_highlight,subset=["MUC2 interaction probability", "+bile"]))    
